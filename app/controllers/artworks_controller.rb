58c9a2fa8c46641158c6f997bc9534b8238544b4
loic poosing <poosingloic@gmail.com>
2023-06-09 12:27:52 +0200

    basic gallery

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -4,6 +4,13 @@ class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
     @sample = @artworks.sample(15)
+    # if params[:query].present?
+    #   @artworks = @artworks.where("artist ILIKE ?", "%#{params[:query]}")
+    # end
+    if params[:query].present?
+      sql_subquery = "title ILIKE :query OR artist ILIKE :query"
+      @artworks = @artworks.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 
   def show
