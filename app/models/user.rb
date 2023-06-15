ecfcfed9b1d46646679ddc8895d8bdaa50300daf
david nigou <melaglue@gmail.com>
2023-06-15 17:02:20 +0200

    dependent destroy user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :username, presence: true
   # validates :email, presence: true
-  has_many :players
+  has_many :players, dependent: :destroy
 end
