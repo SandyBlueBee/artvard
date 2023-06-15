ff92e631572c6f1bebaf34ff13a11b5554207fc9
david nigou <melaglue@gmail.com>
2023-06-15 12:01:53 +0200

    flipcardagain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_093151) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_15_085516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_093151) do
     t.bigint "gameroom_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "creator", default: false
     t.index ["gameroom_id"], name: "index_players_on_gameroom_id"
     t.index ["user_id"], name: "index_players_on_user_id"
   end
@@ -49,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_093151) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "username"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
